50eebd4a68907240de78ad26bf4ec7804ccad4ec
Tai-Jin Lee <taijin@gmail.com>
2013-11-03 14:03:09 -0800

    moving main to base dir to have access to bower components, adding bootstrap-less

--- web/main.js
+++ web/main.js
@@ -1,13 +1,13 @@
 var config = {
   paths: {
-    'backbone': 'lib/backbone/backbone-amd',
-    'bootstrap': 'lib/bootstrap/bootstrap',
-    'json2': 'lib/json2',
-    'jquery': 'lib/jquery/jquery-1.8.3',
-    'select2': 'lib/jquery/select2',
+    'backbone': 'bower_components/backbone/backbone-amd',
+    'bootstrap': 'bower_components/bootstrap/bootstrap',
+    'json2': 'bower_components/json2',
+    'jquery': 'bower_components/jquery/jquery-1.8.3',
+    'select2': 'bower_components/jquery/select2',
     // 'socket.io': '/socket.io/socket.io',
-    'text': 'lib/require/text',
-    'underscore': 'lib/underscore/underscore',
+    'text': 'bower_components/require/text',
+    'underscore': 'bower_components/underscore/underscore',
   },
   shim: {
     'autocomplete': ['jquery'],
