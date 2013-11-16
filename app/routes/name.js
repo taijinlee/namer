dc14ef7b5d4fa99098bfbb29a9085d8874c3c01d
Tai-Jin Lee <taijin@gmail.com>
2013-11-15 19:31:40 -0800

    removing extra whitespace

--- app/routes/name.js
+++ app/routes/name.js
@@ -1,10 +1,7 @@
-
-
 module.exports = function(socket, store) {
   var nameModel = require(process.env.APP_ROOT + '/app/models/name.js')(store);
 
   socket.on('name:create', function(data, callback) {
     nameModel.insert(data, callback);
   });
-
 };
