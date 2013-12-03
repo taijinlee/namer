0a041fb56e4481d169af5e512804dd8648f4d0e1
Tai-Jin Lee <taijin@gmail.com>
2013-12-02 18:19:05 -0800

    locking down user route so that you can only read self

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,10 @@ module.exports = function(socket, store) {
   var userModel = require(process.env.APP_ROOT + '/app/models/user.js')(store);
 
   socket.on('user:read', function(data, callback) {
-    userModel.retrieve(data, callback);
+    if (data.id !== socket.handshake.userId) {
+      return callback(new Error('Cannot read other user'));
+    }
+
+    return userModel.retrieve(data, callback);
   });
 };
